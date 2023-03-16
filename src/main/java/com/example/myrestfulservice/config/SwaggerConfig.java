e223a00fbc7ccf497ee23466ab2a501ef51ae0f4
dongah.shin <dongah.shin@hanmi.co.kr>
2023-03-16 16:14:53 +0900

    -

--- src/main/java/com/example/myrestfulservice/config/SwaggerConfig.java
+++ src/main/java/com/example/myrestfulservice/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +17,18 @@ import java.util.List;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private static final Contact DEFAULT_CONTACT = new Contact("Kenneth Lee",
+            "http://www.joneconsulting.co.kr"
+            , "edowon@joneconsulting.co.kr"
+            ,"1.0"
+            , "urn:tos"
+            , DEFAULT_CONTACT
+            , "Apache 2.0"
+            , "http://www.apache.org/licenses/LICENSE-2.0"
+            , new ArrayList<>()
+            );
+
+
 
     @Bean
     public Docket api(){
