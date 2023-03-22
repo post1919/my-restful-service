841e2cf79135b6d14ebcad8d393c6cabff3b11ac
dongah.shin <post1919@naver.com>
2023-03-22 21:45:49 +0900

    h2데이터베이스 설정 추가

--- src/main/java/com/example/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/com/example/myrestfulservice/config/SecurityConfig.java
@@ -3,15 +3,26 @@ package com.example.myrestfulservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**")
+                .permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("username")
-                .password("{noop}dashin3!")
+                .withUser("dongah.shin")
+                .password("{noop}1111")
                 .roles("USER");
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users values (1, now(), 'user1', 'test111', '830107-11010112');
+insert into users values (2, now(), 'user2', 'test222', '930107-11010112');
+insert into users values (3, now(), 'user3', 'test333', '630107-11010112');
